3ded912ca434209765d9cfb9dc3082b50f5c825f
bikkio <38795852+bikkio@users.noreply.github.com>
2018-05-28 13:42:56 +0200

    Update SkyInstallScriptStart.sh

--- SkyInstallScriptStart.sh
+++ SkyInstallScriptStart.sh
@@ -133,9 +133,9 @@ sleep 5
 cd $GOPATH/bin
 ###### Starts Skywire Monitor service for the first time / after that command web interface of Monitor works
 ###### This line is to start the monitor on the master node (OrangePI 1) only / Other Orange PIs requiere a different line with the master nodes IP!!!
-./node -connect-manager -manager-address :5998 -manager-web :8000 -discovery-address messenger.skycoin.net:5999 -address :5000 -web-port :6001 &> /dev/null 2>&1 &
+./node -connect-manager -manager-address 192.168.1.101:5998 -manager-web 192.168.1.101:8000 -discovery-address discovery.skycoin.net:5999-034b1cd4ebad163e457fb805b3ba43779958bba49f2c5e1e8b062482904bacdb68 -address :5000 -web-port :6001 > /dev/null 2>&1 &
 echo "Skywire monitor started." 
-echo "You can now open the monitor in your browser: http://192.168.0.101:8000"
+echo "You can now open the monitor in your browser: http://192.168.1.101:8000"
 sleep 10
 
 
